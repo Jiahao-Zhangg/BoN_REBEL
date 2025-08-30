80e3004ec5a7cbbf6a947b04e05ee623038fcfd7
Keltin Grimes <keltin.grimes@gmail.com>
2025-08-30 00:40:20 -0400

    Added new judge types

--- src/ultrafeedback_judge/checklist_judge.py
+++ src/ultrafeedback_judge/checklist_judge.py
@@ -1,3 +1,7 @@
+import os
+from collections import Counter
+from pathlib import Path
+
 from datasets import load_dataset, Dataset
 from tqdm import tqdm
 from transformers import AutoTokenizer
@@ -11,72 +15,14 @@ import random
 import numpy as np
 
 
-REWARD_PROMPT = """Please examine the following prompt-response pair and provide a score from 0 to 100 for the quality of the response. Please check the key criteria question to determine the score.
-
-Prompt:
-'''
-{prompt}
-'''
-
-Key Criteria Question:
-'''
-{check}
-'''
-
-Response:
-'''
-{response}
-'''
-
-Respond *only* with the numerical score.
-"""
-
 REWARD_GUIDED_DECODING = GuidedDecodingParams(choice=[str(i) for i in range(101)])
 
-def parse_reward(text):
-    try:
-        result = int(text)
-        if result < 0 or result > 100:
-            return None
-        return result
-    except:
-        return None
+PREFERENCE_BINARY_GUIDED_DECODING = GuidedDecodingParams(choice=["A", "B"])
 
+PREFERENCE_TERNARY_GUIDED_DECODING = GuidedDecodingParams(choice=["A", "B", "Tie"])
 
-PREFERENCE_PROMPT = """Please examine the following prompt and two potential responses (A and B). Select the response with the highest quality. Please check the key criteria question to determine the choice.
+PREFERENCE_SCORE_GUIDED_DECODING = GuidedDecodingParams(choice=[str(i) for i in range(11)])
 
-Prompt:
-'''
-{prompt}
-'''
-
-Key Criteria Question:
-'''
-{check}
-'''
-
-Response A:
-'''
-{response_a}
-'''
-
-Response B:
-'''
-{response_b}
-'''
-
-Respond *only* with the letter of the response with the highest quality. Ties are not allowed.
-"""
-
-PREFERENCE_GUIDED_DECODING = GuidedDecodingParams(choice=["A", "B"])
-
-def parse_preference(text):
-    if text.lower() == "a":
-        return "A"
-    elif text.lower() == "b":
-        return "B"
-    else:
-        return None
 
 ## You can also do more complex guided decoding with Pydantic. E.g.:
 # from pydantic import BaseModel, Field
@@ -95,105 +41,115 @@ def set_seed(seed=5775709):
     torch.cuda.manual_seed_all(seed)
 
 
+def get_winner(values):
+    counts = Counter(values)
+    counts = {k: counts.get(k, 0) for k in ["A", "B", "Tie"]}
+
+    max_count = max(counts.values())
+    winners = [k for k, v in counts.items() if v == max_count]
+
+    # Case 1: all three tied
+    if len(winners) == 3:
+        return "Tie"
+
+    # Case 2: Tie between 'Tie' and another letter -> return the letter
+    if "Tie" in winners and len(winners) == 2:
+        return next(k for k in winners if k != "Tie")
+
+    # Case 3: Tie between 'a' and 'b' (no 'Tie') -> return 'Tie'
+    if set(winners) == {"A", "B"}:
+        return "Tie"
+
+    # Otherwise: unique mode
+    return winners[0]
+
+
 def parse_arguments():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--judge_model", type=str, default="meta-llama/Llama-3.2-3B-Instruct")
-    # parser.add_argument("--judge_model", type=str, default="meta-llama/Llama-3.1-8B-Instruct")
-    parser.add_argument("--judge_type", type=str, default="reward", choices=["reward", "preference"])
-    parser.add_argument("--input_repo", type=str, default="viswavi/rlcf")
+    parser.add_argument("--judge_model", type=str, default="Qwen/Qwen2.5-72B-Instruct")
+    parser.add_argument("--judge_type", type=str, default="reward", choices=["reward", "preference_binary", "preference_ternary", "preference_score"])
+    parser.add_argument("--input_repo", type=str, default="viswavi/wildchecklists")
     parser.add_argument("--selection_pairs", type=int, default=2, help="number of pairs to use for selecting chosen/reject responses")
     parser.add_argument("--gradient_pairs", type=int, default=0, help="number of pairs to use for gradient estimation")
     parser.add_argument("--max_tokens", type=int, default=20, help="max tokens to generate by the judge model")
     parser.add_argument("--world_size", type=int, default=2)
+
+    parser.add_argument("--n_reward_samples", type=int, default=5)
+    parser.add_argument("--temperature", type=float, default=1.3)
+    parser.add_argument("--top_p", type=float, default=0.9)
+
     return parser.parse_args()
 
 
 def get_message(instruction):
-    message = [
-        {"role": "user", "content": instruction},
-    ]
-    return message
+    return [{"role": "user", "content": instruction}]
 
 
-def main():
-    # init
-    st = time.time()
-    args = parse_arguments()
-
-
-    # dataset
-    dataset = load_dataset(args.input_repo, split='train')
-    dataset = dataset.select(range(10))
-    
-    # Split requirements and create new rows
-    expanded_data = []
-    for row in dataset:
-        requirements = list(map(lambda x: x[3:].strip(), row['requirements'].split('\n')))
-        for req in requirements:
-            if req.startswith('Does the response satisfy the following two criteria:'):
-                continue
-            new_row = dict(row)
-            new_row['check'] = req.split('(importance:')[0].strip()
-            new_row['response_0'] = dataset['chosen']
-            new_row['response_1'] = dataset['rejected']
-            expanded_data.append(new_row)
-    # Create new dataset from expanded data
-    dataset = Dataset.from_list(expanded_data)
-
-    # load model
-    tokenizer = AutoTokenizer.from_pretrained(args.judge_model)
-    llm = LLM(
-        model=args.judge_model,
-        tensor_parallel_size=args.world_size,
-    )
-
-    total_pairs = args.selection_pairs + args.gradient_pairs
-    for i in range(0, total_pairs, 1 if args.judge_type == "reward" else 2):
+def judge(
+    llm, tokenizer, judge_type, prompt_template, guided_decoding, dataset, total_pairs, 
+    max_tokens, world_size, n_reward_samples, temperature, top_p
+):
+    for i in range(0, total_pairs, 1 if judge_type.startswith("reward") else 2):
         print(f'gathering reward for {i+1}th response')
 
         # prompts for judge
-        if args.judge_type == "reward":
+        if judge_type.startswith("reward"):
             prompts = [
                 get_message(
-                    REWARD_PROMPT.format(prompt=row['prompt'], response=row[f'response_{i}'], check=row['check'])
+                    prompt_template.format(
+                        prompt=row['prompt'], 
+                        response=row[f'response_{i}'][1]["content"], 
+                        check=row['check']
+                    )
                 ) for row in tqdm(dataset)
             ]
-        elif args.judge_type == "preference":
+        elif judge_type.startswith("preference"):
             prompts = [
                 get_message(
-                    PREFERENCE_PROMPT.format(prompt=row['prompt'], response_a=row[f'response_{i}'], response_b=row[f'response_{i+1}'], check=row['check'])
+                    prompt_template.format(
+                        prompt=row['prompt'], 
+                        response_a=row[f'response_{i}'][1]["content"], 
+                        response_b=row[f'response_{i+1}'][1]["content"], 
+                        check=row['check']
+                    )
                 ) for row in tqdm(dataset)
             ]
         prompts = [tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True) for prompt in prompts]
 
         # start generate
         set_seed(0)
-        guided = REWARD_GUIDED_DECODING if args.judge_type == "reward" else PREFERENCE_GUIDED_DECODING
         sampling_params = SamplingParams(
-            temperature=0.0,
-            max_tokens=args.max_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            n=n_reward_samples,
+            max_tokens=max_tokens,
             seed=0,
-            guided_decoding=guided,
+            guided_decoding=guided_decoding,
         )
         response = llm.generate(prompts, sampling_params)
 
         # add to dataset
-        if args.judge_type == "reward":
-            output = list(map(lambda x: x.outputs[0].text, response))
-            output = list(map(parse_reward, output))
+        output = list(map(lambda x: [r.text for r in x.outputs], response))    # Get responses
+        output = list(map(lambda x: [r for r in x if r is not None], output))  # Filter out None's
+
+        if judge_type.startswith("reward") or judge_type == "preference_score":
+            output = list(map(lambda x: [int(r) for r in x], output))
+            output = list(map(lambda x: sum(x) / len(x) if len(x) > 0 else None, output))  # Average the rewards or scored preferences
+        elif judge_type.startswith("preference"):
+            output = list(map(lambda x: get_winner(x), output))  # Pick most common answer
+
+        if judge_type.startswith("reward"):
             dataset = dataset.add_column(f"response_{i}_judged_reward", output)
             # filter out invalid judgements (None's)
             dataset = dataset.filter(lambda row: row[f"response_{i}_judged_reward"] is not None)
-        elif args.judge_type == "preference":
-            output = list(map(lambda x: x.outputs[0].text, response))
-            output = list(map(parse_preference, output))
+        elif judge_type.startswith("preference"):
             dataset = dataset.add_column(f"response_{i}_{i+1}_judged_preference", output)
             # filter out invalid judgements (None's)
             dataset = dataset.filter(lambda row: row[f"response_{i}_{i+1}_judged_preference"] is not None)
 
     # Merge dataset by 'prompt': combine response reward columns into lists
-    if args.judge_type == "reward":
+    if judge_type.startswith("reward"):
         # Group by prompt and merge the judged reward columns
         merged_data = {}
         
@@ -207,17 +163,17 @@ def main():
                     if f'response_{i}' in row:
                         merged_data[prompt][f'response_{i}'] = []
             
-            # Append judged rewards to the corresponding response lists
+            # Append judged rewards and importance to the corresponding response lists
             for i in range(total_pairs):
                 if f'response_{i}_judged_reward' in row and row[f'response_{i}_judged_reward'] is not None:
                     if f'response_{i}' not in merged_data[prompt]:
                         merged_data[prompt][f'response_{i}'] = []
                     merged_data[prompt][f'response_{i}'].append(row[f'response_{i}_judged_reward'])
-        
+
         # Convert back to dataset
         dataset = Dataset.from_list(list(merged_data.values()))
-    
-    elif args.judge_type == "preference":
+
+    elif judge_type.startswith("preference"):
         # Group by prompt and merge the judged preference columns
         merged_data = {}
         
@@ -240,10 +196,89 @@ def main():
         
         # Convert back to dataset
         dataset = Dataset.from_list(list(merged_data.values()))
+
+    return dataset
+
+
+def main():
+    # init
+    st = time.time()
+    args = parse_arguments()
+
+    # prompt template
+    if args.judge_type == "reward":
+        filename = "prompt_reward.txt"
+        guided_decoding = REWARD_GUIDED_DECODING
+    elif args.judge_type == "preference_binary":
+        filename = "prompt_preference_binary.txt"
+        guided_decoding = PREFERENCE_BINARY_GUIDED_DECODING
+    elif args.judge_type == "preference_ternary":
+        filename = "prompt_preference_ternary.txt"
+        guided_decoding = PREFERENCE_TERNARY_GUIDED_DECODING
+    elif args.judge_type == "preference_score":
+        filename = "prompt_preference_score.txt"
+        guided_decoding = PREFERENCE_SCORE_GUIDED_DECODING
+    with open(Path(__file__).parent / filename, "r") as f:
+        prompt_template = f.read()
+
+    # dataset
+    dataset = load_dataset(args.input_repo, split='train', download_mode="force_redownload")
     
-    # dataset.push_to_hub(args.input_repo + f'_judge_{args.judge_type}')
-    dataset.push_to_hub(f"MisDrifter/play_with_checklist_{args.judge_type}")
+    # Split requirements and create new rows
+    expanded_data = []
+    for row in dataset:
+        # Loop and extract requirements
+        requirements_str: str = row['requirements']
+        counter = 1
+        requirements = []
+        while len(requirements_str) > 0:
+            assert requirements_str.startswith(f"{counter})")
+            if requirements_str.find(f"/100)\n{counter+1})") > 0:
+                curr_requirement = requirements_str[len(f"{counter})"):requirements_str.find(f"/100)\n{counter+1})") + len("/100)\n")]
+            else:
+                curr_requirement = requirements_str[len(f"{counter})"):]
+            requirements.append(curr_requirement)
+            requirements_str = requirements_str[len(curr_requirement) + len(f"{counter})"):]
+            counter += 1
+        requirements = list(map(lambda x: x.strip(), requirements))
+
+        for req in requirements:
+            new_row = dict(row)
+            new_row['check'] = req.split('(importance:')[0].strip()
+            new_row['importance'] = int(req.split('(importance:')[1].split('/')[0].strip())
+            new_row['response_0'] = row['chosen']
+            new_row['response_1'] = row['rejected']
+            expanded_data.append(new_row)
+
+    # Create new dataset from expanded data
+    dataset = Dataset.from_list(expanded_data)
+
+    # load model
+    tokenizer = AutoTokenizer.from_pretrained(args.judge_model)
+    llm = LLM(
+        model=args.judge_model,
+        tensor_parallel_size=args.world_size,
+    )
+
+    total_pairs = args.selection_pairs + args.gradient_pairs
+    dataset = judge(
+        llm,
+        tokenizer,
+        args.judge_type,
+        prompt_template,
+        guided_decoding,
+        dataset,
+        total_pairs,
+        args.max_tokens,
+        args.world_size,
+        args.n_reward_samples,
+        args.temperature,
+        args.top_p,
+    )
+
+    dataset.push_to_hub(args.input_repo + f'_judge_{args.judge_type}')
     print(f'time taken: {time.time() - st}')
+
 
 if __name__ == "__main__":
     main()
