f79f4d8dd830a64957662d473b131317ac84a2c5
Jiahao-Zhangg <jiahaozhang@cmu.edu>
2025-11-25 13:50:12 -0500

    change port

--- sw_scripts/7b_multi_iter2_eta_1e4.sh
+++ sw_scripts/7b_multi_iter2_eta_1e4.sh
@@ -85,7 +85,7 @@ set -u
 TRAIN_CMD=(
     python -m accelerate.commands.launch
     --config_file accelerate_cfgs/deepspeed_config_stage_3.yaml
-    --main-process-port 29080
+    --main-process-port 29083
     --num_processes "$WORLD_SIZE"
     src/ultrafeedback_judge/rebel_save.py
     --output_dir "$OUTPUT_DIR"
